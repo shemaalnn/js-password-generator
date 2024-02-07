4468d04eb54c1e7fcbd5ea2e9b12a2750dd2bcda
Shema <shema.aln@outlook.com>
2024-02-07 00:11:00 +0000

    password generation added

---
+++ script.js
@@ -0,0 +1,71 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword () {
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var specialCharacters = "!'#$%&()+*,-./:;<>=?@[]\^_`~{}|"
+  var numbers = "01234567890"
+
+  var uppercasePreference = confirm("Do you want uppercase letters in your password?")
+  var lowercasePreference = confirm("Do you want lowercase letters in your password?")
+  var numbersPreference = confirm("Do you want numbers in your password?")
+  var specialCharactersPreference = confirm("Do you want any special characters in your password?")
+
+  if (!uppercasePreference && !lowercasePreference && !numbersPreference && !specialCharactersPreference ) {
+    alert("You must have atleast one character type in your password!");
+    return "";
+  }
+
+  var passwordLength = prompt("How long do you want your password to be?")
+
+  if (typeof passwordLength === 'number') {
+    alert("Thank you!")
+  } else {
+    alert("Please choose your password length")
+  }
+
+  if (passwordLength < 7 || passwordLength > 120) {
+    alert("Your password must be between 7 and 120 characters in length")
+    return "";
+  }
+
+  var passwordCharacters = "";
+
+  if (lowercasePreference) {
+    passwordCharacters += lowercase
+  }
+
+  if (uppercasePreference) {
+    passwordCharacters += uppercase
+  }
+
+  if (numbersPreference) {
+    passwordCharacters += numbers
+  }
+
+  if (specialCharactersPreference) {
+    passwordCharacters += specialCharacters
+  }
+
+  var password = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
+  }
+
+  console.log(password)
+  return password
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
